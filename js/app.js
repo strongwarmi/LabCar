6ca848707c5ac1d8afdbf827f0f74d962fa5c1c4
strongwarmi <elianasc10@hotmail.com>
2017-05-29 17:01:26 -0500

    agregue la seccion info en desktop y mobile

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var menuEscondido = document.getElementByID("menu-vertical");
+var menuEscondido = document.getElementById('menu-vertical');
 hamburguer.addEventListener("click",function(){
     menuEscondido.style.display="block";
 });
