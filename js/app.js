fa56575c79a47fd606ced9175d995761c2cac168
strongwarmi <elianasc10@hotmail.com>
2017-05-29 18:49:12 -0500

    agregando seccion map

--- js/app.js
+++ js/app.js
@@ -2,3 +2,43 @@ var menuEscondido = document.getElementById('menu-vertical');
 hamburguer.addEventListener("click",function(){
     menuEscondido.style.display="block";
 });
+function initMap() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+// autocompleta los inputs con la direccion que se escriba
+var inputPartida=document.getElementById("inputPartida");
+var inputDestino=document.getElementById("inputDestino");
+new google.maps.places.Autocomplete(inputPartida);
+new google.maps.places.Autocomplete(inputDestino);
+// Try HTML5 geolocation.
+if (navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(function(position) {
+           var pos = {
+             lat: position.coords.latitude,
+             lng: position.coords.longitude
+           };
+
+           infoWindow.setPosition(pos);
+           infoWindow.setContent('Location found.');
+           map.setCenter(pos);
+         }, function() {
+           handleLocationError(true, infoWindow, map.getCenter());
+         });
+       } else {
+         // Browser doesn't support Geolocation
+         handleLocationError(false, infoWindow, map.getCenter());
+       }
+}//fin initmap
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+      }
